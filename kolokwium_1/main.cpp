d5fbafa513d269362793c1001b1d64dfae14bf5a
kari <90532831+malywojcik@users.noreply.github.com>
2023-12-11 16:48:42 +0100

    cyran poprawka

--- kolokwium_1/main.cpp
+++ kolokwium_1/main.cpp
@@ -4,33 +4,35 @@
 
 using namespace std;
 
-int serie(int x[], int y[], int z[], const int n)
+int serie(const int x[], int y[], int z[], const int n)
 {
     int ilosc_serii = 0;
     bool ktora = false; // false do tab2, true do tab3
-    int dl_tab2 = 0, dl_tab3 = 0;
+    int dl_tab2 = -1, dl_tab3 = -1, last_tab2 = 0, last_tab3 = 0;
     for (int i = 0; i < n-1; i++)
     {
-        if (x[i] > *(x + 1))
+        if (x[i] > x[i + 1])
         {
             ilosc_serii++;
             if(!ktora)
             {
-                for (int j = dl_tab2; j <= i; j++)
+                for (int j = last_tab3; j <= i; j++)
                 {
-                    y[j] = x[j];
+                    dl_tab2++;
+                    y[dl_tab2] = x[j];
                 }
                 ktora = true; // zminana na  tab3
-                dl_tab2 = i++;
+                last_tab2 = i + 1;
             }
             else
             {
-                for (int j = dl_tab3; j <= i; j++)
+                for (int j = last_tab2; j <= i; j++)
                 {
-                    z[j] = x[j];
+                    dl_tab3++;
+                    z[dl_tab3] = x[j];
                 }
                 ktora = false; // zmiana na tab2
-                dl_tab3 = i++;
+                last_tab3 = i + 1;
             }
             continue;
         }
@@ -40,7 +42,7 @@ int serie(int x[], int y[], int z[], const int n)
 
 int main()
 {
-    srand(time(NULL));
+    srand(time(nullptr));
     const int n = 20;
     int tab1[n], tab2[n], tab3[n];
     for (int i = 0; i < n; i++)
@@ -50,11 +52,25 @@ int main()
         tab3[i] = 0;
     }
     cout << endl;
-    for (int i = 0; i < n; i++)
+    for (int i : tab1)
     {
-        cout << tab1[i] << "  ";
+        cout << i << "  ";
     }
     cout << endl << endl << "ilosc serii: " << serie(tab1, tab2, tab3, n) << endl;
 
+    for (int i : tab2)
+    {
+        if(i == 0)
+            break;
+        cout << i << "  ";
+    }
+    cout << endl;
+    for (int i : tab3)
+    {
+        if(i == 0)
+            break;
+        cout << i << "  ";
+    }
+cout << endl;
     return 0;
 }
